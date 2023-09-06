7a8c2ede97e3add035fbbfdbf7b9654b3d8f41a7
OscarOsornio04 <99107962+OscarOsornio04@users.noreply.github.com>
2023-09-05 20:05:25 -0600

    Practica 4
    Practica de visualización del tamaño de los datos.

---
+++ primer parcial/practicas/p4/Practica4/Practica4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tipo int Tamaño: {0} Rango [{1},{2}]", sizeof(int), int.MinValue, int.MaxValue);
+            Console.WriteLine("tipo float Tamaño: {0} Rango [{1},{2}]", sizeof(float), float.MinValue, float.MaxValue);
+            Console.WriteLine("tipo double Tamaño: {0} Rango [{1},{2}]", sizeof(double), double.MinValue, double.MaxValue);
+            Console.WriteLine("tipo Int16 Tamaño: {0} Rango [{1},{2}]", sizeof(Int16), Int16.MinValue, Int16.MaxValue);
+            Console.WriteLine("tipo Int32 Tamaño: {0} Rango [{1},{2}]", sizeof(Int32), Int32.MinValue, Int32.MaxValue);
+            Console.WriteLine("tipo Double Tamaño: {0} Rango [{1},{2}]", sizeof(Double), Double.MinValue, Double.MaxValue);
+            Console.WriteLine("tipo Boolean Tamaño: {0}", sizeof(Boolean));
+            Console.WriteLine("tipo bool Tamaño: {0}", sizeof(bool));
+            Console.WriteLine("tipo char Tamaño: {0} Rango [{1},{2}]", sizeof(char), char.MinValue, char.MaxValue);
+        }
+    }
+}
