94ebf3279b92e936706025c5e619eb8a0c0bad8e
OscarOsornio04 <99107962+OscarOsornio04@users.noreply.github.com>
2023-09-15 14:33:20 -0600

    Practica 9
    Practica que abre aplicaciones por medio de un PictureBox

---
+++ primer parcial/practicas/practica9/LanzadorApp_Practica9/LanzadorApp_Practica9/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace LanzadorApp_Practica9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.firmaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pBbloc = new System.Windows.Forms.PictureBox();
+            this.pBvisual = new System.Windows.Forms.PictureBox();
+            this.pBedge = new System.Windows.Forms.PictureBox();
+            this.pBreloj = new System.Windows.Forms.PictureBox();
+            this.pBword = new System.Windows.Forms.PictureBox();
+            this.pBexcel = new System.Windows.Forms.PictureBox();
+            this.pBpp = new System.Windows.Forms.PictureBox();
+            this.pBvideos = new System.Windows.Forms.PictureBox();
+            this.pBfotos = new System.Windows.Forms.PictureBox();
+            this.pBcalculadora = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBbloc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBvisual)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBedge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBreloj)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBexcel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBpp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBvideos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBfotos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBcalculadora)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.firmaToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // firmaToolStripMenuItem
+            // 
+            this.firmaToolStripMenuItem.Name = "firmaToolStripMenuItem";
+            this.firmaToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.firmaToolStripMenuItem.Text = "Firma";
+            this.firmaToolStripMenuItem.Click += new System.EventHandler(this.firmaToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pBbloc);
+            this.groupBox1.Controls.Add(this.pBvisual);
+            this.groupBox1.Controls.Add(this.pBedge);
+            this.groupBox1.Controls.Add(this.pBreloj);
+            this.groupBox1.Controls.Add(this.pBword);
+            this.groupBox1.Controls.Add(this.pBexcel);
+            this.groupBox1.Controls.Add(this.pBpp);
+            this.groupBox1.Controls.Add(this.pBvideos);
+            this.groupBox1.Controls.Add(this.pBfotos);
+            this.groupBox1.Controls.Add(this.pBcalculadora);
+            this.groupBox1.Location = new System.Drawing.Point(27, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(367, 427);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sistema";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(250, 396);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Reloj";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(50, 396);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Explorador";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(251, 317);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label8.Size = new System.Drawing.Size(32, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Edge";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(50, 317);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Bloc de Notas";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(248, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Fotos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Videos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(232, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Power Point";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Excel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Word";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Calculadora";
+            // 
+            // pBbloc
+            // 
+            this.pBbloc.Image = ((System.Drawing.Image)(resources.GetObject("pBbloc.Image")));
+            this.pBbloc.Location = new System.Drawing.Point(31, 264);
+            this.pBbloc.Name = "pBbloc";
+            this.pBbloc.Size = new System.Drawing.Size(100, 50);
+            this.pBbloc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBbloc.TabIndex = 9;
+            this.pBbloc.TabStop = false;
+            this.pBbloc.Click += new System.EventHandler(this.pBbloc_Click);
+            // 
+            // pBvisual
+            // 
+            this.pBvisual.Image = ((System.Drawing.Image)(resources.GetObject("pBvisual.Image")));
+            this.pBvisual.Location = new System.Drawing.Point(31, 343);
+            this.pBvisual.Name = "pBvisual";
+            this.pBvisual.Size = new System.Drawing.Size(100, 50);
+            this.pBvisual.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBvisual.TabIndex = 8;
+            this.pBvisual.TabStop = false;
+            this.pBvisual.Click += new System.EventHandler(this.pBvisual_Click);
+            // 
+            // pBedge
+            // 
+            this.pBedge.Image = ((System.Drawing.Image)(resources.GetObject("pBedge.Image")));
+            this.pBedge.Location = new System.Drawing.Point(218, 264);
+            this.pBedge.Name = "pBedge";
+            this.pBedge.Size = new System.Drawing.Size(100, 50);
+            this.pBedge.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBedge.TabIndex = 7;
+            this.pBedge.TabStop = false;
+            this.pBedge.Click += new System.EventHandler(this.pBedge_Click);
+            // 
+            // pBreloj
+            // 
+            this.pBreloj.Image = ((System.Drawing.Image)(resources.GetObject("pBreloj.Image")));
+            this.pBreloj.Location = new System.Drawing.Point(218, 343);
+            this.pBreloj.Name = "pBreloj";
+            this.pBreloj.Size = new System.Drawing.Size(100, 50);
+            this.pBreloj.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBreloj.TabIndex = 6;
+            this.pBreloj.TabStop = false;
+            this.pBreloj.Click += new System.EventHandler(this.pBreloj_Click);
+            // 
+            // pBword
+            // 
+            this.pBword.Image = ((System.Drawing.Image)(resources.GetObject("pBword.Image")));
+            this.pBword.Location = new System.Drawing.Point(218, 19);
+            this.pBword.Name = "pBword";
+            this.pBword.Size = new System.Drawing.Size(100, 50);
+            this.pBword.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBword.TabIndex = 5;
+            this.pBword.TabStop = false;
+            this.pBword.Click += new System.EventHandler(this.pBword_Click);
+            // 
+            // pBexcel
+            // 
+            this.pBexcel.Image = ((System.Drawing.Image)(resources.GetObject("pBexcel.Image")));
+            this.pBexcel.Location = new System.Drawing.Point(31, 99);
+            this.pBexcel.Name = "pBexcel";
+            this.pBexcel.Size = new System.Drawing.Size(100, 50);
+            this.pBexcel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBexcel.TabIndex = 4;
+            this.pBexcel.TabStop = false;
+            this.pBexcel.Click += new System.EventHandler(this.pBexcel_Click);
+            // 
+            // pBpp
+            // 
+            this.pBpp.Image = ((System.Drawing.Image)(resources.GetObject("pBpp.Image")));
+            this.pBpp.Location = new System.Drawing.Point(218, 99);
+            this.pBpp.Name = "pBpp";
+            this.pBpp.Size = new System.Drawing.Size(100, 50);
+            this.pBpp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBpp.TabIndex = 3;
+            this.pBpp.TabStop = false;
+            this.pBpp.Click += new System.EventHandler(this.pBpp_Click);
+            // 
+            // pBvideos
+            // 
+            this.pBvideos.Image = ((System.Drawing.Image)(resources.GetObject("pBvideos.Image")));
+            this.pBvideos.Location = new System.Drawing.Point(31, 179);
+            this.pBvideos.Name = "pBvideos";
+            this.pBvideos.Size = new System.Drawing.Size(100, 50);
+            this.pBvideos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBvideos.TabIndex = 2;
+            this.pBvideos.TabStop = false;
+            this.pBvideos.Click += new System.EventHandler(this.pBvideos_Click);
+            // 
+            // pBfotos
+            // 
+            this.pBfotos.Image = ((System.Drawing.Image)(resources.GetObject("pBfotos.Image")));
+            this.pBfotos.Location = new System.Drawing.Point(218, 179);
+            this.pBfotos.Name = "pBfotos";
+            this.pBfotos.Size = new System.Drawing.Size(100, 50);
+            this.pBfotos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBfotos.TabIndex = 1;
+            this.pBfotos.TabStop = false;
+            this.pBfotos.Click += new System.EventHandler(this.pBfotos_Click);
+            // 
+            // pBcalculadora
+            // 
+            this.pBcalculadora.Image = ((System.Drawing.Image)(resources.GetObject("pBcalculadora.Image")));
+            this.pBcalculadora.Location = new System.Drawing.Point(31, 19);
+            this.pBcalculadora.Name = "pBcalculadora";
+            this.pBcalculadora.Size = new System.Drawing.Size(100, 50);
+            this.pBcalculadora.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBcalculadora.TabIndex = 0;
+            this.pBcalculadora.TabStop = false;
+            this.pBcalculadora.Click += new System.EventHandler(this.pBcalculadora_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 486);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lanzador";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBbloc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBvisual)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBedge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBreloj)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBexcel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBpp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBvideos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBfotos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBcalculadora)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem firmaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pBbloc;
+        private System.Windows.Forms.PictureBox pBvisual;
+        private System.Windows.Forms.PictureBox pBedge;
+        private System.Windows.Forms.PictureBox pBreloj;
+        private System.Windows.Forms.PictureBox pBword;
+        private System.Windows.Forms.PictureBox pBexcel;
+        private System.Windows.Forms.PictureBox pBpp;
+        private System.Windows.Forms.PictureBox pBvideos;
+        private System.Windows.Forms.PictureBox pBfotos;
+        private System.Windows.Forms.PictureBox pBcalculadora;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ primer parcial/practicas/practica9/LanzadorApp_Practica9/LanzadorApp_Practica9/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LanzadorApp_Practica9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firmaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Proyecto1.exe");
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void pBcalculadora_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("calc.exe");
+        }
+
+        private void pBword_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("WINWORD.EXE");
+        }
+
+        private void pBexcel_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("EXCEL.EXE");
+        }
+
+        private void pBpp_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("POWERPNT.EXE");
+        }
+
+        private void pBvideos_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("wmplayer.exe");
+        }
+
+        private void pBfotos_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("ms-photos:");
+        }
+
+        private void pBbloc_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("notepad.exe");
+        }
+
+        private void pBedge_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("msedge.exe");
+        }
+
+        private void pBvisual_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe");
+        }
+
+        private void pBreloj_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("ms-clock:");
+        }
+    }
+}
